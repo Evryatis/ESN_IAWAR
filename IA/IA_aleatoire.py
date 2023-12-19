4098c37be9f8a9653337185ea82bf8193409a70f
Evryatis <48336818+Evryatis@users.noreply.github.com>
2023-12-19 15:46:58 +0100

    Update IA_aleatoire.py

--- IA/IA_aleatoire.py
+++ IA/IA_aleatoire.py
@@ -26,52 +26,73 @@ class IA_ESNW:
         Args:
             game_dict (dict): descriptif de l'état de la partie
 
-        Returns:
-            str : une action 'N', 'S', 'E', 'W', 'L', 'E', 'P'
+            str : une action 'N', 'S', '
+        Returns:E', 'W', 'L', 'E', 'P'
 
         """
 
         coders = game_dict['coders']
+
         current_player_number = 0  # Replace with the actual player number of your AI
 
         # Access the AI's current position
         current_position = coders[current_player_number]['position']
+        current_energy = coders[current_player_number]['energy']
+        current_money = coders[current_player_number]['bitcoins']
 
         # Access the missions' positions from the game_dict
         missions_positions = {tuple(m['position']): m for m in game_dict['missions']}
 
-        for mission_position, mission in missions_positions.items(): # Vérifie constemment la mission la plus proche du joueur
-            if mission_position != current_position:
-                position_x = current_position[0]
-                position_y = current_position[1]
-                position_x_vise = 0 # Variable x de la mission la plus proche
-                position_y_vise = 0 # Variable y de la mission la plus proche
+        if current_energy == 0:
+            position_x_vise = 10
+            position_y_vise = 10
+            if current_position[0] != position_x_vise:
+                if current_position[0] > position_x_vise:
+                    return 'E'
+                else:
+                    return 'W'
+            if current_position[1] != position_y_vise:
+                if current_position[1] > position_y_vise:
+                    return 'N'
+                else:
+                    return 'S'
+            if current_money > 100:
+                return 'L'
+            return 'R'
 
-                for x, y in [mission_position]: # Calcule la différence de proximité entre la position visée et la meilleure position..
+
+        if current_energy > 0:
+
+            position_x_vise = float('inf')  # Initialize with positive infinity to ensure any mission position is closer
+            position_y_vise = float('inf')
+
+            for mission_position, mission in missions_positions.items():
+
+                if mission_position != current_position:
+
+                    x, y = mission_position
+
                     differencex = abs(x - current_position[0])
                     differencey = abs(y - current_position[1])
 
-                    differencex1 = abs(x - position_x_vise)
-                    differencey1 = abs(y - position_y_vise)
-
-                    # Verifie si la mission vérifiée est plus proche ou pas de la current mission visée
-                    if differencex + differencey < differencex1 + differencey1:
+                    # Check if the current mission is closer than the previously targeted mission
+                    if differencex + differencey < abs(position_x_vise - current_position[0]) + abs(position_y_vise - current_position[1]):
                         position_x_vise = x
                         position_y_vise = y
 
-        
-        if current_position[0] != position_x_vise: # Vérifie si la coordonée x du joueur est en accords avec la coordonnée x de la mission
-            if current_position[0] > position_x_vise:
-                return 'E'
-            else:
-                return 'W'
-        if current_position[1] != position_y_vise: # Vérifie si la coordonée y du joueur est en accords avec la coordonnée y de la mission
-            if current_position[1] > position_y_vise:
-                return 'N'
-            else:
-                return 'S'
-        if current_position == (position_x_vise, position_y_vise):
-            return 'P'
+
+            if current_position[0] != position_x_vise: # Vérifie si la coordonée x du joueur est en accords avec la coordonnée x de la mission
+
+                if current_position[0] > position_x_vise:
+                    return 'E'
+                else:
+                    return 'W'
+            if current_position[1] != position_y_vise: # Vérifie si la coordonée y du joueur est en accords avec la coordonnée y de la mission
+                if current_position[1] > position_y_vise:
+                    return 'N'
+                else:
+                    return 'S'
+
 
     def game_over(self, game_dict: dict) -> None:
         """Appelé à la fin du jeu ; sert à ce que vous voulez
@@ -80,5 +101,3 @@ class IA_ESNW:
             descr (str): descriptif du dernier tour de jeu
         """
         pass
-
-
