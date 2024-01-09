1ee6dafcd202124456dbfe708e6cf97dac96ace3
Evryatis <48336818+Evryatis@users.noreply.github.com>
2024-01-09 10:28:51 +0100

    Update IA_aleatoire.py

--- IA/IA_aleatoire.py
+++ IA/IA_aleatoire.py
@@ -1,6 +1,7 @@
 import random
 from moteur_esn_wars import *
 
+
 class IA_ESNW:
     def __init__(self, num_joueur: int, game_dict: dict) -> None:
         """Initialize the AI object.
@@ -32,69 +33,70 @@ class IA_ESNW:
         # Access the missions' positions from the game_dict
         missions_positions = {tuple(m['position']): m for m in game_dict['missions']}
 
-        if current_energy == 0: # Si l'energie est à 0
-            position_x_vise = 10 # On vise le game center
+        if current_energy == 0:  # Si l'energie est à 0
+            position_x_vise = 10  # On vise le game center
             position_y_vise = 10
-            
-            if current_position[0] != position_x_vise: # On se deplace vers le game center
-                
+
+            if current_position[0] != position_x_vise:  # On se deplace vers le game center
+
                 return 'E' if current_position[0] > position_x_vise else 'W'
-            
+
             if current_position[1] != position_y_vise:
-                
                 return 'N' if current_position[1] > position_y_vise else 'S'
-            
-            if current_money > 100:
-                
+
+            if current_money > 100 and current_position[0] == 10 and current_position[1] == 10:
                 return 'L'
-            
+
             return 'P'
 
         # If energy is not 0, et qu'on peut donc travailler
         if current_energy > 0:
 
-            position_x_vise = float('inf') # On initialise une valeur visée infinie positive pour inclure toute mission possible
+            position_x_vise = float(
+                'inf')  # On initialise une valeur visée infinie positive pour inclure toute mission possible
             position_y_vise = float('inf')
 
             for mission_position, mission in missions_positions.items():
-                
-                if mission_position != current_position: # SI on est pas sure une mission :
-                    
-                    x, y = mission_position 
-                    
+
+                if mission_position != current_position:  # Si on est pas sure une mission :
+
+                    x, y = mission_position
+
                     differencex = abs(x - current_position[0])
                     differencey = abs(y - current_position[1])
 
-                    if mission['workload'] < current_energy: # Et que une mission possède un workload inferieur à l'energie :
-                        
+                    # Et que une mission possède un workload inferieur à l'energie, et un cooldown suffisant :
+                    if mission['workload'] < current_energy and mission['cooldown'] < 4:
+
                         position_x_vise = x
                         position_y_vise = y
 
-                        if current_position[0] != position_x_vise: # On bouge vers cette mission
+                        if current_position[0] != position_x_vise:  # On bouge vers cette mission
                             return 'E' if current_position[0] > position_x_vise else 'W'
                         if current_position[1] != position_y_vise:
                             return 'N' if current_position[1] > position_y_vise else 'S'
 
-            for mission_position, mission in missions_positions.items(): # Sinon..
-                
+            for mission_position, mission in missions_positions.items():  # Lance une boucle pour constemment surveiller la liste
+
                 if mission_position != current_position:
-                    
+
                     x, y = mission_position
-                    
-                    differencex = abs(x - current_position[0]) # Verifie la différence entre la position du joueur et la position visée de la mission
+
+                    # Calcule la différence entre la position du joueur, et la position de la mission
+                    differencex = abs(x - current_position[0])
                     differencey = abs(y - current_position[1])
 
-                    if differencex + differencey < abs(position_x_vise - current_position[0]) + abs(position_y_vise - current_position[1]): #jsp ce que ça fait
-                        
+                    # Compare constemment les missions qui sont dans la liste avec la mission visée, si la distance de celle-ci est inférieur, choisi celle là.
+                    if differencex + differencey < abs(position_x_vise - current_position[0]) + abs(position_y_vise - current_position[1]) and mission['cooldown'] < 2:
+
                         position_x_vise = x
                         position_y_vise = y
 
-            if current_position[0] != position_x_vise: # On bouge alors vers cette mission.
-                
+            if current_position[0] != position_x_vise:  # On bouge alors vers cette mission.
+
                 return 'E' if current_position[0] > position_x_vise else 'W'
-            
+
             if current_position[1] != position_y_vise:
-                
                 return 'N' if current_position[1] > position_y_vise else 'S'
 
         return 'P'
